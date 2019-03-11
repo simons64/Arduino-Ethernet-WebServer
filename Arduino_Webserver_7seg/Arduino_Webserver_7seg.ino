cce4fb44cf10afbd9e5c45274a413d0a54aa2582
SimonKasten <30275716+SimonKasten@users.noreply.github.com>
2019-03-11 15:35:38 +0100

    Add files via upload

---
+++ Arduino_Webserver_7seg/Arduino_Webserver_7seg.ino
@@ -0,0 +1,344 @@
+/*-------------------------------------------------------------- 
+  References:   - WebServer example by David A. Mellis and 
+                  modified by Tom Igoe
+                - SD card examples by David A. Mellis and
+                  Tom Igoe
+                - Ethernet library documentation:
+                  http://arduino.cc/en/Reference/Ethernet
+                - SD Card library documentation:
+                  http://arduino.cc/en/Reference/SD
+                - http://startingelectronics.com
+--------------------------------------------------------------*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include "webserver_files.h"
+
+// size of buffer used to capture HTTP requests
+#define REQ_BUF_SZ   200
+
+// size of packages for Ethernet client.write
+#define package_size   256
+
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 178, 222); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
+EthernetClient client;
+
+char HTTP_req[REQ_BUF_SZ] = {0}; // buffered HTTP request stored as null terminated string
+char req_index = 0;              // index into HTTP_req buffer
+boolean LED_state[4] = {0}; // stores the states of the LEDs
+
+void setup()
+{
+    // disable Ethernet chip
+    pinMode(10, OUTPUT);
+    digitalWrite(10, HIGH);
+    
+    Serial.begin(115200);       // for debugging
+    while(!Serial);             // warte auf Serial
+    
+    // switches on pins 2, 3 and 5
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
+    pinMode(5, INPUT);
+    // LEDs
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    
+    Ethernet.begin(mac, ip);  // initialize Ethernet device
+
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+
+    
+    server.begin();           // start to listen for clients
+    
+    Serial.println("Ethernet.localIP(): ");
+    Serial.println(Ethernet.localIP());
+}
+
+void loop()
+{
+    client = server.available();  // try to get client
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read();
+                // leave last element in array as 0 to null terminate string (REQ_BUF_SZ - 1)
+                if (req_index < (REQ_BUF_SZ - 1)) {
+                    HTTP_req[req_index] = c;          // save HTTP request character
+                    req_index++;
+                }
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // display received HTTP request on serial port
+                    Serial.print("\n=====HTTP_req=====\n");
+                    Serial.print(HTTP_req);
+                    Serial.print("\n->HTTP Answer:\n");
+
+                    
+                    // standard http response header
+                    client.println("HTTP/1.1 200 OK");
+
+                    // type of HTTP_REQUEST
+                    if (StrContains(HTTP_req, "ajax_inputs")) {
+                        Serial.println("  ---> ajax_script.js");
+                        client.println("Content-Type: text/xml");
+                        client.println("Connection: keep-alive");
+                        client.println();
+                        SetLEDs();
+                        
+                        XML_response(client);
+                    }
+                    else if (StrContains(HTTP_req, "sevenSeg.js")) {
+                        // send rest of HTTP header
+                        Serial.println("  ---> sevenSeg.js");
+                        client.println("Content-Type: application/javascript");
+                        client.println("Connection: close");
+                        client.println();
+
+                        print_http_answer(sevenSeg_js);
+                    }
+                    else if (StrContains(HTTP_req, "demo.js")) {
+                        // send rest of HTTP header
+                        Serial.println("  ---> demo.js");
+                        client.println("Content-Type: application/javascript");
+                        client.println("Connection: close");
+                        client.println();
+
+                        print_http_answer(demo_js);
+                    }
+                    else if (StrContains(HTTP_req, "prettify.css")) {
+                        // send rest of HTTP header
+                        Serial.println("  ---> prettify.css");
+                        client.println("Content-Type: text/css");
+                        client.println("Connection: close");
+                        client.println();
+
+                        print_http_answer(prettify_css);
+                    }
+                    
+                    else {  // web page request
+                        // send rest of HTTP header
+                        client.println("Content-Type: text/html");
+                        client.println("Connection: close");
+                        client.println();
+                        // send web page
+                        Serial.println("  ---> print index_htm to Site");
+
+                        print_http_answer(index_htm);
+                    }
+                    
+                    // reset buffer index and all buffer elements to 0
+                    req_index = 0;
+                    StrClear(HTTP_req, REQ_BUF_SZ);
+                    Serial.println("==================");
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // web browser time to receive data
+        client.stop(); // close the connection
+    } // end if (client)
+}
+
+
+// checks if received HTTP request is switching on/off LEDs
+// also saves the state of the LEDs
+void SetLEDs(void)
+{
+    // LED 1 (pin 6)
+    if (StrContains(HTTP_req, "LED1=1")) {
+        LED_state[0] = 1;  // save LED state
+        digitalWrite(6, HIGH);
+    }
+    else if (StrContains(HTTP_req, "LED1=0")) {
+        LED_state[0] = 0;  // save LED state
+        digitalWrite(6, LOW);
+    }
+    // LED 2 (pin 7)
+    if (StrContains(HTTP_req, "LED2=1")) {
+        LED_state[1] = 1;  // save LED state
+        digitalWrite(7, HIGH);
+    }
+    else if (StrContains(HTTP_req, "LED2=0")) {
+        LED_state[1] = 0;  // save LED state
+        digitalWrite(7, LOW);
+    }
+    // LED 3 (pin 8)
+    if (StrContains(HTTP_req, "LED3=1")) {
+        LED_state[2] = 1;  // save LED state
+        digitalWrite(8, HIGH);
+    }
+    else if (StrContains(HTTP_req, "LED3=0")) {
+        LED_state[2] = 0;  // save LED state
+        digitalWrite(8, LOW);
+    }
+    // LED 4 (pin 9)
+    if (StrContains(HTTP_req, "LED4=1")) {
+        LED_state[3] = 1;  // save LED state
+        digitalWrite(9, HIGH);
+    }
+    else if (StrContains(HTTP_req, "LED4=0")) {
+        LED_state[3] = 0;  // save LED state
+        digitalWrite(9, LOW);
+    }
+}
+
+// send the XML file with analog values, switch status
+//  and LED status
+void XML_response(EthernetClient cl)
+{
+    int analog_val;            // stores value read from analog inputs
+    int count;                 // used by 'for' loops
+    int sw_arr[] = {2, 3, 5};  // pins interfaced to switches
+    
+    cl.print("<?xml version = \"1.0\" ?>");
+    cl.print("<inputs>");
+    // read analog inputs
+    for (count = 2; count <= 5; count++) { // A2 to A5
+        analog_val = analogRead(count);
+        cl.print("<analog>");
+        cl.print(analog_val);
+        cl.println("</analog>");
+    }
+    // read switches
+    for (count = 0; count < 3; count++) {
+        cl.print("<switch>");
+        if (digitalRead(sw_arr[count])) {
+            cl.print("ON");
+        }
+        else {
+            cl.print("OFF");
+        }
+        cl.println("</switch>");
+    }
+    // checkbox LED states
+    // LED1
+    cl.print("<LED>");
+    if (LED_state[0]) {
+        cl.print("checked");
+    }
+    else {
+        cl.print("unchecked");
+    }
+    cl.println("</LED>");
+    // LED2
+    cl.print("<LED>");
+    
+    if (LED_state[1]) {
+        cl.print("checked");
+    }
+    else {
+        cl.print("unchecked");
+    }
+     cl.println("</LED>");
+    // button LED states
+    // LED3
+    cl.print("<LED>");
+    if (LED_state[2]) {
+        cl.print("on");
+    }
+    else {
+        cl.print("off");
+    }
+    cl.println("</LED>");
+    // LED4
+    cl.print("<LED>");
+    if (LED_state[3]) {
+        cl.print("on");
+    }
+    else {
+        cl.print("off");
+    }
+    cl.println("</LED>");
+    
+    cl.print("</inputs>");
+}
+
+
+void print_http_answer(const char *PRINT_FILE){
+    int len_buf = strlen(PRINT_FILE);
+    int packages = len_buf / package_size;
+    int bytes_left = len_buf;
+
+    Serial.print("  ---> ");
+    Serial.print("len_buf: ");
+    Serial.print(len_buf);
+    Serial.print(" pack_size: ");
+    Serial.print(package_size);
+    Serial.print(" packages: ");
+    Serial.println(packages);
+    Serial.print("  ---> ");
+    
+    for (int i = 0; i < packages; i++){
+        // Verpackt in size = 256 schreiben
+        client.write(PRINT_FILE + (i * package_size) ,  package_size);
+        bytes_left -= 256;
+        Serial.print(".");
+    }
+    // Rest schreiben
+    client.write(PRINT_FILE + (packages * package_size),  bytes_left); 
+    Serial.println(" END");
+}
+
+
+
+// sets every element of str to 0 (clears array)
+void StrClear(char *str, char length)
+{
+    for (int i = 0; i < length; i++) {
+        str[i] = 0;
+    }
+}
+
+// searches for the string sfind in the string str
+// returns 1 if string found
+// returns 0 if string not found
+char StrContains(char *str, char *sfind)
+{
+    char found = 0;
+    char index = 0;
+    char len;
+
+    len = strlen(str);
+    
+    if (strlen(sfind) > len) {
+        return 0;
+    }
+    while (index < len) {
+        if (str[index] == sfind[found]) {
+            found++;
+            if (strlen(sfind) == found) {
+                return 1;
+            }
+        }
+        else {
+            found = 0;
+        }
+        index++;
+    }
+
+    return 0;
+}
---
+++ Arduino_Webserver_7seg/webserver_files.h
@@ -0,0 +1,12 @@
+#ifndef webserver_files_H_   /* Include guard */
+#define webserver_files_H_
+
+
+extern const char *index_htm;
+
+extern const char *sevenSeg_js;
+extern const char *demo_js;
+extern const char *prettify_css;
+
+
+#endif // webserver_files_H_
